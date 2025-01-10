8f63480056c344e866d991105948c6a9c00c740e
avaschmidt <ava.schmidt25@d155.org>
2025-01-10 07:32:38 -0600

    getting temp on the screen

--- APINotes/ViewController.swift
+++ APINotes/ViewController.swift
@@ -5,7 +5,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var tempOutlet: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        getWeather()
     }
 
     func getWeather(){
@@ -16,22 +16,50 @@ class ViewController: UIViewController {
         // making an API call for the weather in Crystal Lake Illinois
         // API Key = f32dd81585c694f7094fb8da758b55b4
         // URL = https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
-        let weatherURL = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=42.2&lon=-88.3&appid=f32dd81585c694f7094fb8da758b55b4")!
+        // you must remove the s from the https
+        // remeber to add stuff to the info section
+        //convert to imperial with &units=imperial
+        let weatherURL = URL(string: "http://api.openweathermap.org/data/2.5/weather?lat=42.2&lon=-88.3&appid=f32dd81585c694f7094fb8da758b55b4&units=imperial")!
         
         // URLRequest is our variable we created (ie. weatherURL)
         // completionHandler is what we want it to do once it has the URL
-        let dataTest = session.dataTask(with: weatherURL) { data, response, error in
+        let dataTask = session.dataTask(with: weatherURL) { data, response, error in
             if let e = error{
                 print("Error! \(e)")
             }
             else{
                 if let d = data{
-                    if let jsonObj = JSONSerialization.jsonObject(with: d, options: .)
+                    // getting the json object from the API
+                    if let jsonObj = try? JSONSerialization.jsonObject(with: d, options: .fragmentsAllowed) as? NSDictionary{
+                        print(jsonObj)
+                        if let mainDictionary = jsonObj.value(forKey: "main") as? NSDictionary{
+                            print(mainDictionary)
+                            if let theTemp =  mainDictionary.value(forKey: "temp"){
+                                print(theTemp)
+                                
+                                // happening on the main thread
+                                DispatchQueue.main.async {
+                                    self.tempOutlet.text = "The temp is \(theTemp)"
+                                }
+                               
+                            }
+                        }
+                    }
+                    else{
+                        print("Can't convert to JSON")
+                    }
+                }
+                else{
+                    print("Could't get data")
                 }
             }
         }
+        // constantly calling for the data
+        dataTask.resume()
+        
     }
     
+   
     
 
 }
